56a6266afcc47a4892c124757ad1cf699e60d4af
hirely2-cyber <hirely2@gmail.com>
2026-04-15 22:35:56 +0700

    Add migration: create pwadmin_cubi_log table

---
+++ database/migrations/2026_04_15_223330_create_pwadmin_cubi_log_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('mysql_game')->create('pwadmin_cubi_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('userid')->index();
+            $table->integer('cash')->default(0);
+            $table->datetime('creatime')->useCurrent()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('mysql_game')->dropIfExists('pwadmin_cubi_log');
+    }
+};
